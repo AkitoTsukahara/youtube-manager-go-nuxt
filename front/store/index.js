2e1971d9a1dde36d73f8eb4b56bca51c2388433e
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-12-20 22:55:03 +0900

    add detail page

--- front/store/index.js
+++ front/store/index.js
@@ -1,6 +1,7 @@
 import {createRequestClient} from '~/store/request-client';
 export const state = () => ({
   items: [],
+  item: {},
   meta: {},
 })
 export const actions = {
@@ -9,12 +10,24 @@ export const actions = {
     const res = await client.get(payload.uri, payload.params)
     commit('mutatePopularVideos', res)
   },
+  async findVideo({commit}, payload) {
+    const client = createRequestClient(this.$axios)
+    const res = await client.get(payload.uri)
+    const params = {
+      ...res.video_list
+    }
+    commit('mutateVideo', params)
+  }
 }
 export const mutations = {
   mutatePopularVideos(state, payload) {
     state.items = payload.items ? state.items.concat(payload.items) : []
     state.meta = payload
   },
+  mutateVideo(state, payload) {
+    const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+    state.item = params
+  }
 }
 export const getters = {
   getPopularVideos(state) {
@@ -22,5 +35,8 @@ export const getters = {
   },
   getMeta(state) {
     return state.meta
+  },
+  getVideo(state) {
+    return state.item
   }
 }
